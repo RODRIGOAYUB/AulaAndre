a77e0acc61e06660921e5fc3b036e20c80ec3e69
RODRIGOAYUB <62073594+RODRIGOAYUB@users.noreply.github.com>
2023-05-17 20:37:25 -0300

    Create python_names_storage.py

---
+++ .github/workflows/python_names_storage.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+from google.cloud import storage
+from google.oauth2 import service_account
+import requests
+import csv
+
+headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+
+credentials_dict = {
+    "type": "service_account",
+  "project_id": "projeto1-382123",
+  "private_key_id": "GOOGZYKDOOL4DX77GLPMY65I",
+  "private_key": "-----BEGIN PRIVATE KEY-----\naG9Oj/L7+V7AShafW85Q8xSV32ahH9ck9ygdb6MD\n-----END PRIVATE KEY-----\n",
+  "client_email": "service-594036329011@compute-system.iam.gserviceaccount.com",
+  "client_id": "116227407277652709047",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/pmcardamone%40moonlit-buckeye-382223.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"  
+}
+
+try:
+
+  """Uploads a file to the bucket."""
+  credentials = service_account.Credentials.from_service_account_info(credentials_dict)
+  storage_client = storage.Client(credentials=credentials)
+  bucket = storage_client.get_bucket('bucketdataops16_05') ### Nome do seu bucket
+  blob = bucket.blob('artist-names.csv')
+
+  pages = []
+  names = "Name \n"
+
+  for i in range(1, 5):
+    url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ' + str(i) + '.htm'
+    pages.append(url)
+
+  for item in pages:
+    page = requests.get(item)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    last_links = soup.find(class_='AlphaNav')
+    last_links.decompose()
+
+    artist_name_list = soup.find(class_='BodyText')
+    artist_name_list_items = artist_name_list.find_all('a')
+
+    for artist_name in artist_name_list_items:
+      names = names + artist_name.contents[0] + "\n"
+
+    blob.upload_from_string(names, content_type="text/csv")
+
+except Exception as ex:
+  print(ex) 
